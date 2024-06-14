a0a7e4a865b29625d3eb2c128d9bb6baa7726306
Gabriel Costa <gabriel11119477@edu.pa.senac.br>
2024-06-13 21:22:14 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Console1{
+    internal class Program{
+        static void Main(string[] args){
+            //Dado padrão que poderia vir de um banco de dados
+            Usuario usuario = new Usuario();
+
+            usuario.nome = "Gabriel";
+            usuario.login = "root";
+            usuario.senha = "qwe123";
+            //------
+            string login;
+            string senha;
+            do{
+                Console.WriteLine("Digite seu login: ");
+                login = Console.ReadLine();
+
+                Console.WriteLine("Digite sua senha: ");
+                senha = Console.ReadLine();
+
+            }while(!usuario.Logar(login, senha));
+        }
+    }
+}
---
+++ Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Console1{
+    class Usuario{
+        public string nome;
+        public string login;
+        public string senha;
+        public int qtdLogin;
+        public float peso;
+        public bool ehSolteiro;
+        public char abrvNomeMeio;
+
+        public bool Logar(string _login, string _senha){
+            if (_login == login){
+                if (_senha == senha){
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
